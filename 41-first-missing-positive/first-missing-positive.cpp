ce04408950fcf4269c60180d919a3e8628b59835
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-14 20:30:04 +0530

    Time: 2 ms (61.28%) | Memory: 55 MB (43.96%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        vector<int>vis(n+1,0);
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>0 && nums[i]<=n){
-                vis[nums[i]] =1;
-            }
+        for(int i=0;i<n;i++){
+           while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+            swap(nums[i],nums[nums[i]-1]);
+           }
         }
-        for(auto ele : vis) cout<<ele<<" ";
-        for(int i=1;i<=n;i++){
-            if(vis[i]==0) return i;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1) return i+1;
         }
         return n+1;
     }
