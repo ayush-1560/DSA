524b9e4b7ed2ba42c1f6f9a81cef710febd8c9a7
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-21 19:24:29 +0530

    Time: 3 ms (88.94%) | Memory: 127.8 MB (67.41%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<pair<int,int>>smaller;
-        vector<pair<int,int>>equal;
-        vector<pair<int,int>>larger;
-        for(int i=0;i<n;i++){
-            if(nums[i]<pivot) smaller.push_back({i,nums[i]});
-            else if(nums[i]==pivot) equal.push_back({i,nums[i]});
-            else larger.push_back({i,nums[i]});
-        }
+        vector<int>ans(n);
         int idx=0;
-
-        for(auto ele : smaller) {
-            nums[idx] = ele.second;
-            idx++;
+        for(int i =0;i<n;i++){
+            if(nums[i]<pivot){
+                ans[idx] =nums[i];
+                idx++;
+            }
         }
-        for(auto ele : equal) {
-            nums[idx] = ele.second;
-            idx++;
+        for(int i =0;i<n;i++){
+            if(nums[i]==pivot){
+                ans[idx] =nums[i];
+                idx++;
+            }
         }
-        for(auto ele : larger) {
-            nums[idx] = ele.second;
-            idx++;
+        for(int i =0;i<n;i++){
+            if(nums[i]>pivot){
+                ans[idx] =nums[i];
+                idx++;
+            }
         }
-        return nums;
+        return ans;
     }
 };
