606dc3a7e84e5dbe4ef002928fc98e035679871e
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-23 14:03:23 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (91.86%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -4,23 +4,26 @@ public:
     int countServers(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<int>rowCount(m,0);
-        vector<int>colCount(n,0);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) rowCount[i]++;
-            }
-        }
-        for(int j=0;j<n;j++){
-            for(int i=0;i<m;i++){
-                if(grid[i][j]==1) colCount[j]++;
-            }
-        }
         int serverCount=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    if(rowCount[i] > 1 || colCount[j]>1) serverCount++;
+                    bool canComm=false;
+                    for(int k=0;k<n;k++){
+                        if(grid[i][k]==1 && k!=j){
+                            serverCount++;
+                            canComm=true;
+                            break;
+                        }
+                    }
+                    if(!canComm){
+                        for(int t=0;t<m;t++){
+                        if(grid[t][j]==1 && t!=i){
+                            serverCount++;
+                            break;
+                        }
+                    }
+                    }
                 }
             }
         }
