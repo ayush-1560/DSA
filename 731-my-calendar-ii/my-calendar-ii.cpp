5eadf6d845cbee2dcbb9b898cd2b52ab59777837
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-23 22:44:14 +0530

    Time: 90 ms (54.67%) | Memory: 44.1 MB (40.48%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.cpp
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -1,30 +1,28 @@
 class MyCalendarTwo {
 public:
-    vector<pair<int,int>>doubleOverLap;
-    vector<pair<int,int>>bookings;
-
-    bool isOverLap(int start1, int end1, int start2, int end2){
-        return (max(start1,start2)<min(end1,end2));
-    }
-    pair<int,int> findOverLapRegion(int start1, int end1, int start2, int end2){
-        return {max(start1,start2),min(end1,end2)};
-    }
+    map<int,int>mp;
     MyCalendarTwo() {
         
     }
     
     bool book(int start, int end) {
-        for(auto region : doubleOverLap){
-            if(isOverLap(region.first,region.second,start,end)){
+        mp[start]++;
+        mp[end]--;
+        int sum=0;
+        for(auto it : mp){
+            sum+=it.second;
+            if(sum>2){
+                mp[start]--;
+                mp[end]++;
                 return false;
             }
         }
-        for(auto booking : bookings){
-            if(isOverLap(booking.first,booking.second,start,end)){
-                doubleOverLap.push_back(findOverLapRegion(booking.first,booking.second,start,end));
-            }
-        }
-        bookings.push_back({start,end});
         return true;
     }
 };
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
