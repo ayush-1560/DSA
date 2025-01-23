3ef7a980dcf1dfd3d62c35abe5cfa923849fdfa2
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-23 15:27:48 +0530

    Time: 6 ms (25.00%) | Memory: 30.8 MB (11.27%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
@@ -3,10 +3,14 @@ public:
     int numberOfPoints(vector<vector<int>>& nums) {
         int maxi=INT_MIN;
         for(auto query : nums) maxi=max({maxi,query[0],query[1]});
-        vector<bool>vis(maxi+1,false);
-        for(auto query : nums)for(int i=query[0];i<=query[1];i++) vis[i]=true;
-        int ans=0;
-        for(auto ele : vis) if(ele) ans++;
-        return ans;
+        vector<int>line(maxi+2,0);
+        for(auto query : nums){
+            line[query[0]]++;
+            line[query[1]+1]--;
+        }
+        int count=0;
+        for(int i=1;i<line.size();i++) line[i]+=line[i-1];
+        for(auto ele : line) if(ele) count++;
+        return count;
     }
 };
