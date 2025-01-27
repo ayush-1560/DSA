308e711e858d5de779b19f0e6d247a297d9e88cc
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-27 21:39:54 +0530

    Time: 451 ms (5.08%) | Memory: 77.1 MB (26.75%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -11,18 +11,29 @@ public:
         }
         return isReachable;
     }
+    void preProcess(int n, unordered_map<int,vector<int>>&adj,
+    vector<vector<bool>>&isPreq){
+        for(int u=0;u<n;u++){
+            for(int v=0;v<n;v++){
+                if(u==v) continue;
+                vector<bool>vis(n,false);
+                if(dfs(adj,u,v,vis)) isPreq[u][v]=true;
+            }
+        }
+    }
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
         unordered_map<int,vector<int>>adj;
         for(auto ele : prerequisites){
             adj[ele[0]].push_back(ele[1]);
         }
+        vector<vector<bool>>isPreq(numCourses,vector<bool>(numCourses,false));
+        preProcess(numCourses,adj,isPreq);
         int n = queries.size();
         vector<bool>ans(n,false);
         for(int i =0;i<n;i++){
             int u = queries[i][0];
             int v = queries[i][1];
-            vector<bool>vis(numCourses,false);
-            ans[i] = dfs(adj,u,v,vis);
+            ans[i] =isPreq[u][v];
         }
         return ans;
     }
