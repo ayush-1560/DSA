a69e36ee3531324225ad5760b33028ccc3ab8f50
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-27 21:58:37 +0530

    Time: 92 ms (33.94%) | Memory: 80.3 MB (19.46%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>>&adj,int u,int v,vector<bool>&vis){
-        vis[u] = true;
-        if(u==v) return true;
-        bool isReachable = false;
-        for(auto it : adj[u]){
-            if(!vis[it]){
-                isReachable = isReachable ||  dfs(adj,it,v,vis);
-            }
-        }
-        return isReachable;
-    }
-    void preProcess(int n, unordered_map<int,vector<int>>&adj,
-    vector<vector<bool>>&isPreq){
-        for(int u=0;u<n;u++){
-            for(int v=0;v<n;v++){
-                if(u==v) continue;
-                vector<bool>vis(n,false);
-                if(dfs(adj,u,v,vis)) isPreq[u][v]=true;
-            }
-        }
-    }
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
         unordered_map<int,vector<int>>adj;
+        vector<int>inDegree(numCourses,0);
         for(auto ele : prerequisites){
             adj[ele[0]].push_back(ele[1]);
+            inDegree[ele[1]]++;
         }
-        vector<vector<bool>>isPreq(numCourses,vector<bool>(numCourses,false));
-        preProcess(numCourses,adj,isPreq);
+        queue<int>q;
+        for(int i=0;i<inDegree.size();i++){
+            if(!inDegree[i]) q.push(i);
+        }
+        unordered_map<int,unordered_set<int>>mp;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it : adj[node]){
+                mp[it].insert(node);
+                for(auto prereq : mp[node]){
+                    mp[it].insert(prereq);
+                }
+                inDegree[it]--;
+                if(inDegree[it]==0) q.push(it);
+            }
+        }
         int n = queries.size();
         vector<bool>ans(n,false);
-        for(int i =0;i<n;i++){
-            int u = queries[i][0];
-            int v = queries[i][1];
-            ans[i] =isPreq[u][v];
+        int i =0;
+        for(auto query : queries){
+            ans[i] = mp[query[1]].contains(query[0]);
+            i++;
         }
         return ans;
     }
