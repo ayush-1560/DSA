232d6c2b070fcc8bf0f79879c6b0488ed8fe4bf6
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-28 08:45:01 +0530

    Time: 11 ms (51.14%) | Memory: 95.9 MB (69.18%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    int bfs(vector<vector<int>>& grid,int i ,int j){
-        int m = grid.size();
-        int n = grid[0].size();
-        int sum=0;
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        sum+=grid[i][j];
-        grid[i][j]=0;
-        while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-            for(auto dir : directions){
-                int i_ = i + dir[0];
-                int j_ = j+ dir[1];
-                if(i_>=0 && i_<m && j_>=0 && j_<n && grid[i_][j_]>0){
-                    sum+=grid[i_][j_];
-                    grid[i_][j_]=0;
-                    q.push({i_,j_});
-                }
-            }
-       }
-       return sum;
-    }
+    int m,n;
     vector<vector<int>>directions = {{0,1},{0,-1},{1,0},{-1,0}};
+    int dfs(vector<vector<int>>& grid,int i , int j){
+      if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0) return 0;
+      int sum=grid[i][j];
+      grid[i][j]=0;
+      for(auto& dir : directions){
+        int i_= i+ dir[0];
+        int j_= j+ dir[1];
+        sum+=dfs(grid,i_,j_);
+      }
+      return sum;
+    }
     int findMaxFish(vector<vector<int>>& grid) {
-       int m = grid.size();
-       int n = grid[0].size();
+       m = grid.size();
+       n = grid[0].size();
        int ans = 0;
        for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            if(grid[i][j]>0) ans= max(ans,bfs(grid,i,j));
+            if(grid[i][j]>0) ans= max(ans,dfs(grid,i,j));
          }
        }
        return ans;
