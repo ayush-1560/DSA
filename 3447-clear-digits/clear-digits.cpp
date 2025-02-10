d1e9ba258f3a8fb03a34bc16f4f349fa72bdac4d
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-02-10 15:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.42%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        int n = s.length();
-        unordered_set<int>st;
-        for(int i=n-1;i>=0;i--){
-            if(isdigit(s[i])){
-                for(int j=i-1;j>=0;j--){
-                    if(!isdigit(s[j]) && !st.count(j)){
-                        st.insert(j);
-                        break;
-                    }
-                }
-            }
-        }
         string ans="";
-        for(int i=0;i<n;i++){
-            if(!st.count(i) && !isdigit(s[i])) ans+=s[i];
+        for(char ch : s){
+            if(isdigit(ch)) ans.pop_back();
+            else ans+=ch;
         }
         return ans;
     }
