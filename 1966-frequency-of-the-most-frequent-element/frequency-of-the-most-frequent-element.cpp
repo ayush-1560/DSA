e82037a49587e3513cc6e545b92de356dec0b8f9
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-02-10 20:38:56 +0530

    Time: 35 ms (76.93%) | Memory: 105.8 MB (60.96%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    int bs(int target_idx,int k,vector<int>& nums,vector<long>&pre){
-        int target=nums[target_idx];
-        int l=0,r=target_idx,ans=target_idx;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            long count = (target_idx -mid +1);
-            long windowSum = count*target;
-            long currSum = pre[target_idx] - pre[mid] + nums[mid];
-            int ops = windowSum-currSum;
-            if(ops > k){
-                l=mid+1;
-            }
-            else{
-                ans=mid;
-                r=mid-1;
-            }
-        }
-        return (target_idx-ans+1);
-    }
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        vector<long>pre(n);
-        pre[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pre[i]=pre[i-1]+nums[i];
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans=max(ans,bs(i,k,nums,pre));
+        int l=0,ans=0;
+        long currSum=0;
+        for(int r=0;r<n;r++){
+            long target=nums[r];
+            currSum+=nums[r];
+            if(((r-l+1)*target-currSum)>k){
+                currSum-=nums[l];
+                l++;
+            }
+            ans= max(ans,r-l+1);
         }
         return ans;
     }
