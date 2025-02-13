57f16b5f169692aed60a0ac18841c3d4c5de1f25
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-02-13 08:05:31 +0530

    Time: 91 ms (81.17%) | Memory: 99.2 MB (16.98%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -4,12 +4,11 @@ public:
         priority_queue<long long,vector<long long>,greater<>>pq;
         for(auto ele : nums) pq.push(ele);
         int ops =0;
-        while(pq.size()>1){
+        while(pq.size()>1 && pq.top()<k){
             long long x = pq.top();
             pq.pop();
             long long y = pq.top();
             pq.pop();
-            if(x>=k) return ops;
             ops++;
             pq.push(x*2+y);
         }
