764385191ba0fe67da9b04aa3ed7966d1397039f
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-02-16 22:31:26 +0530

    Time: 33 ms (39.94%) | Memory: 66 MB (44.10%) - LeetSync

--- 1582-design-browser-history/design-browser-history.cpp
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -1,33 +1,44 @@
+class Node{
+    public:
+    string val;
+    Node* next;
+    Node* prev;
+    Node(string val){
+        this->val=val;
+        this->next=this->prev=NULL;
+    }
+};
 class BrowserHistory {
 public:
-    stack<string>past;
-    stack<string>future;
-    string curr;
+    Node* currentPage;
     BrowserHistory(string homepage) {
-        curr=homepage;
+        currentPage = new Node(homepage);
     }
     
     void visit(string url) {
-        past.push(curr);
-        curr=url;
-        future=stack<string>();
+        Node* newPage = new Node (url);
+        currentPage->next=newPage;
+        newPage->prev = currentPage;
+        currentPage = currentPage->next;
     }
     
     string back(int steps) {
-        while(steps-- && !past.empty()){
-            future.push(curr);
-            curr=past.top();
-            past.pop();
+        while(steps--){
+            if(currentPage->prev){
+                currentPage = currentPage->prev;
+            }
+            else break;
         }
-        return curr;
+        return currentPage->val;
     }
     
     string forward(int steps) {
-         while(steps-- && !future.empty()){
-            past.push(curr);
-            curr=future.top();
-            future.pop();
+        while(steps--){
+            if(currentPage->next){
+                currentPage = currentPage->next;
+            }
+            else break;
         }
-        return curr;
+        return currentPage->val;
     }
 };
