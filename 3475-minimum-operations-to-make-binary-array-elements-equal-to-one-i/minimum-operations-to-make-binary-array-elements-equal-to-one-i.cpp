306f34dd30bcfb7ffe1a93bd5d483af6faec02e4
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-03-19 16:48:59 +0530

    Time: 3 ms (89.05%) | Memory: 139.9 MB (74.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int solve(vector<int>& nums,int k){
         int n = nums.size();
-        vector<bool>isFlipped(n,false);
         int currFlip=0;
         int flips =0;
         for(int i=0;i<n;i++){
-            if(i>=k && isFlipped[i-k]==true){
+            if(i>=k && nums[i-k]==5){
                 currFlip--;
             }
             if(currFlip%2==nums[i]) {
                 if(i+k>n) return -1;
                 currFlip++;
                 flips++;
-                isFlipped[i]=true;
+                nums[i]=5;
             }
         }
         return flips;
