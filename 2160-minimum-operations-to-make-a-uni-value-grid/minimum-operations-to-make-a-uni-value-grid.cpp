cff57c4aa763fb07078e08a051c7a77541a1d875
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-03-26 16:04:59 +0530

    Time: 13 ms (92.40%) | Memory: 90.1 MB (34.95%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-    int findAns(vector<int>&v,int idx,int x){
-        int ans=0;
-        for(int i=0;i<v.size();i++){
-            int diff = abs(v[idx]-v[i]);
-            if(diff%x!=0) return -1;
-            ans+=diff/x;
-        }
-        return ans;
-    }
     int minOperations(vector<vector<int>>& grid, int x) {
         vector<int>v;
         int m = grid.size();
@@ -18,8 +9,14 @@ public:
                 v.push_back(grid[i][j]);
             }
         }
-        sort(v.begin(),v.end());
         int s = v.size();
-        return findAns(v,s/2,x);
+        nth_element(v.begin(),v.begin()+s/2,v.end());
+        int ans =0;
+        int target = v[s/2];
+        for(auto ele : v){
+            if(ele%x!=target%x) return -1;
+            ans+=abs(ele-target)/x;
+        }
+        return ans;
     }
 };
