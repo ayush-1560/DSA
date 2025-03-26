6fbdbe742be1e3054c90f9fced3f6b6203f9641a
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-03-26 12:06:13 +0530

    Time: 51 ms (31.31%) | Memory: 90 MB (62.01%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    int findDiff(vector<int>&v,int idx1, int idx2){
-        int ops1 = 0, ops2=0;
-        for(int i=0;i<v.size();i++){
-            ops1+=abs(v[idx1]-v[i]);
-        }
-        for(int i=0;i<v.size();i++){
-            ops2+=abs(v[idx2]-v[i]);
-        }
-        if(ops1<ops2) return idx1;
-        else return idx2;
-    }
     int findAns(vector<int>&v,int idx,int x){
         int ans=0;
         for(int i=0;i<v.size();i++){
             int diff = abs(v[idx]-v[i]);
             if(diff%x!=0) return -1;
-            while(diff!=0){
-                diff-=x;
-                ans++;
-            }
+            ans+=diff/x;
         }
         return ans;
     }
@@ -34,7 +20,6 @@ public:
         }
         sort(v.begin(),v.end());
         int s = v.size();
-        int idx = s>=3 ? findDiff(v,s/2,s/2-1) : 0;
-        return findAns(v,idx,x);
+        return findAns(v,s/2,x);
     }
 };
