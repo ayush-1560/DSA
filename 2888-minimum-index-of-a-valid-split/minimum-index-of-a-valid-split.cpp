96e27934ea0d42a46d29ec44203f9fb049070564
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-03-27 10:29:07 +0530

    Time: 4 ms (77.46%) | Memory: 90 MB (70.90%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>mp1;
-        unordered_map<int,int>mp2;
-        for(int num : nums) mp2[num]++;
+        int maj = -1;
+        int count = 0;
         for(int i=0;i<n;i++){
-            int num = nums[i];
-            mp1[num]++;
-            mp2[num]--;
-            int n1 = i+1, n2= n-i-1;
-            if(mp1[num]*2>n1 && mp2[num]*2>n2) return i;
+            if(count==0){
+                maj = nums[i];
+                count = 1;
+            }
+            else if (nums[i]==maj) count++;
+            else count--;
+        }
+        int majCount = 0;
+        for(int num : nums){
+            if(num==maj) majCount++;
+        }
+        count = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==maj) count++;
+            int remCount = majCount - count;
+            int n1 = i+1 , n2 = n-i-1;
+            if(count*2> n1 && remCount*2>n2){
+                return i;
+            }
         }
         return -1;
     }
