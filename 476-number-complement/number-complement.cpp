847f8dd4ca90926f23662a1dab5e20a2a9e8929b
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-03-27 16:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (43.73%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int findComplement(int num) {
         int bits = (int)(log2(num)) + 1;
-        for(int i=0;i<bits;i++){
-            num=num^(1<<i);
-        }
-        return num;
+        unsigned int mask = (1U<<bits) -1;
+        return num^mask;
     }
 };
