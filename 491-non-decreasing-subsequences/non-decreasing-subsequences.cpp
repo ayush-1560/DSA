61ed65db6b17c569668e4159e00a80dbfa902806
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-04-17 14:46:11 +0530

    Time: 31 ms (65.05%) | Memory: 30 MB (39.50%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    set<vector<int>>st;
     void solve(int idx,vector<int>& nums,vector<int>&temp){
-        if(idx>=nums.size()){
-            if(temp.size()>=2) st.insert(temp);
-            return;
+        if(temp.size()>=2){
+            ans.push_back(temp);
         }
-        if(temp.empty() || temp.back()<=nums[idx]){
-            temp.push_back(nums[idx]);
-            solve(idx+1,nums,temp);
-            temp.pop_back();
+        unordered_set<int>st;
+        for(int i=idx;i<nums.size();i++){
+            if((temp.empty() || temp.back()<=nums[i]) && !st.count(nums[i])){
+                temp.push_back(nums[i]);
+                solve(i+1,nums,temp);
+                temp.pop_back();
+                st.insert(nums[i]);
+            }
         }
-        solve(idx+1,nums,temp);
     }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         vector<int>temp;
         solve(0,nums,temp);
-        for(auto vec : st) ans.push_back(vec);
         return ans;
     }
 };
