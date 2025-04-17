622b1f7c97190dd2e3c0a9a3c2bea050f043ff79
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-04-17 12:51:13 +0530

    Time: 2 ms (37.60%) | Memory: 11.1 MB (43.80%) - LeetSync

--- 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
+++ 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int>ans;
-    void solve(int n , int k,int num,int length){
-        if(length==n){
+    void solve(int n , int k,int num){
+        int len = log10(num)+1;
+        if(len==n){
             ans.push_back(num);
             return;
         }
         int lastdigit = num%10;
         if(lastdigit+k<=9){
-            solve(n,k,num*10 + (lastdigit+k),length+1);
+            solve(n,k,num*10 + (lastdigit+k));
         }
         if(k!=0 && lastdigit-k>=0){
-            solve(n,k,num*10 + (lastdigit-k),length+1);
+            solve(n,k,num*10 + (lastdigit-k));
         }
     }
     vector<int> numsSameConsecDiff(int n, int k) {
         for(int i=1;i<=9;i++){
-            solve(n,k,i,1);
+            solve(n,k,i);
         }
         return ans;
     }
