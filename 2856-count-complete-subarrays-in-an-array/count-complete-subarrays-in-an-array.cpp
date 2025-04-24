d31d7fd5acda070b9f017c22c2e9617309df5dbe
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-04-24 10:36:20 +0530

    Time: 17 ms (72.69%) | Memory: 41.9 MB (59.44%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        unordered_set<int>st(nums.begin(),nums.end());
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        int sz = st.size();
+        int i=0,j=0,count = 0;
+        while(j<n){
+            mp[nums[j]]++;
+            while(mp.size()==sz){
+                count+=(n-j);
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+};
