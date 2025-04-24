686b34812ad45dbd98e5e2fcf7bafb19a74430c1
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-04-24 10:37:27 +0530

    Time: 17 ms (72.69%) | Memory: 41.7 MB (71.22%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -4,11 +4,10 @@ public:
         unordered_set<int>st(nums.begin(),nums.end());
         unordered_map<int,int>mp;
         int n = nums.size();
-        int sz = st.size();
         int i=0,j=0,count = 0;
         while(j<n){
             mp[nums[j]]++;
-            while(mp.size()==sz){
+            while(mp.size()==st.size()){
                 count+=(n-j);
                 mp[nums[i]]--;
                 if(mp[nums[i]]==0) mp.erase(nums[i]);
