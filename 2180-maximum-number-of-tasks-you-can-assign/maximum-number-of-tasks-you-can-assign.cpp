3d253ca656808488ae8f9c556431a13006d121f4
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-05-21 15:24:06 +0530

    Time: 815 ms (34.32%) | Memory: 286.2 MB (32.51%) - LeetSync

--- 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.cpp
+++ 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool check(vector<int>& tasks, vector<int>& workers, int pills, int strength, int mid) {
-        int pillsUsed = 0;
-        multiset<int> st(begin(workers), begin(workers) + mid); //best mid workers
+        multiset<int> st(begin(workers), begin(workers) + mid);
 
         for(int i = mid-1; i >= 0; i--) {
             int reqrd = tasks[i];
@@ -10,46 +9,35 @@ public:
 
             if(*it >= reqrd) {
                 st.erase(it);
-            } else if(pillsUsed >= pills) {
+            } else if(pills<=0) {
                 return false;
             } else {
-                //find the weakest worker which can do this strong task using pills
                 auto weakestWorkerIt = st.lower_bound(reqrd - strength);
                 if(weakestWorkerIt == st.end()) {
                     return false;
                 }
                 st.erase(weakestWorkerIt);
-                pillsUsed++;
+                pills--;
             }
         }
 
         return true;
     }
     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
-        int m = tasks.size();
-        int n = workers.size();
-
+        int m = tasks.size(),n=workers.size();
         int l = 0;
-        int r = min(m, n);
-
+        int r = min(m,n);
+        int ans=0;
         sort(begin(tasks), end(tasks));
         sort(begin(workers), end(workers), greater<int>());
-
-        int result = 0;
-
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-
-            if(check(tasks, workers, pills, strength, mid)) {
-                result = mid;
-                l = mid+1;
-            } else {
-                r = mid-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(check(tasks,workers,pills,strength,mid)){
+                ans=mid;
+                l=mid+1;
             }
+            else r=mid-1;
         }
-
-        return result;
-
-
+        return ans;
     }
 };
