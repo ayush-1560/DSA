0d1cc7d87e5020c9e9a0a1841451f59d0af1a4fa
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-06-01 12:15:57 +0530

    Time: 3 ms (65.77%) | Memory: 115.7 MB (12.73%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-
-    int find(vector<int>& tops, vector<int>& bottoms, int val) {
-        int n = tops.size();
-
-        int swapTop = 0;
-        int swapBottom = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(tops[i] != val && bottoms[i] != val) {
-                return -1;
-            } else if(tops[i] != val) {
-                swapTop++;
-            } else if(bottoms[i] != val) {
-                swapBottom++;
-            }
+    int find(vector<int>& tops, vector<int>& bottoms,int val){
+        int swapTops=0,swapBottoms=0;
+        for(int i=0;i<tops.size();i++){
+            if(tops[i]!=val && bottoms[i]!=val) return -1;
+            else if(tops[i]!=val) swapTops++;
+            else if(bottoms[i]!=val) swapBottoms++;
         }
-
-        return min(swapTop, swapBottom);
+        return min(swapTops,swapBottoms);
     }
-
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int result = INT_MAX;
-
-        for(int val = 1; val <= 6; val++) {
-            int swaps = find(tops, bottoms, val);
-
-            if(swaps != -1) {
-                result = min(result, swaps);
-            }
+        int ans =INT_MAX;
+        for(int k=1;k<=6;k++){
+            int steps = find(tops,bottoms,k);
+            if(steps!=-1) ans = min(ans,steps);
         }
-
-        return result == INT_MAX ? -1 : result;
+        return ans==INT_MAX ? -1 : ans;
     }
 };
