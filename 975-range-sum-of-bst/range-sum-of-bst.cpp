2051b35db6ceb217cd4adea4f311192c54a0ca0e
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-03 00:49:24 +0530

    Time: 0 ms (100.00%) | Memory: 64.9 MB (91.67%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -1,15 +1,18 @@
 
 class Solution {
 public:
-    void inorder(TreeNode* root,int low, int high,int& sum){
-        if(!root) return;
-        inorder(root->left,low,high,sum);
-        if(root->val >=low && root->val<=high) sum+=root->val;
-        inorder(root->right,low,high,sum);
+    int inorder(TreeNode* root,int low, int high,int& sum){
+        if(!root) return 0 ;
+        if(root->val >=low && root->val<=high){
+            return root->val + inorder(root->left,low,high,sum)
+            +inorder(root->right,low,high,sum);
+        }
+        else if(root->val < low) return inorder(root->right,low,high,sum);
+        else if(root->val > high) return inorder(root->left,low,high,sum);
+        return sum;
     }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum =0;
-        inorder(root,low,high,sum);
-        return sum;
+        int sum=0;
+        return inorder(root,low,high,sum);
     }
 };
