a270f2e6232101ea4aeee9d27b598c3602879726
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-03 21:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (90.95%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string str = "a";
-        string gen = "b";
-        while(str.length()<k){
-            str+=gen;
-            string temp="";
-            for(char ch : gen){
-                temp+=(ch == 'z') ? 'a' : ch + 1;
-            }
-            gen+=temp;
-        }
-        return str[k-1];
+        return 'a' + __builtin_popcount(k-1);
     }
 };
