a507f80256e37718b3585992dbf75436199b9ff9
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-08 09:58:40 +0530

    Time: 166 ms (47.44%) | Memory: 122.1 MB (69.66%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int n;
+    int bs(vector<vector<int>>& events,int idx){
+        int i=idx;
+        int j=n-1;
+        while(i<=j){
+            int mid = i+ (j-i)/2;
+            if(events[mid][0]>events[idx][1]) j=mid-1;
+            else i=mid+1;
+        }
+        return i;
+    }
     int solve(vector<vector<int>>& events, int k,int idx,vector<vector<int>>&dp){
         if(idx>=n || k==0) return 0;
         if(dp[idx][k]!=-1) return dp[idx][k];
         int skip = solve(events,k,idx+1,dp);
-        int nextIdx=n;
-        for(int j=idx+1;j<n;j++){
-            if(events[j][0]>events[idx][1]){
-                nextIdx=j;
-                break;
-            }
-        }
+        int nextIdx=bs(events,idx);
         int take = events[idx][2]+solve(events,k-1,nextIdx,dp);
         return  dp[idx][k]=max(take,skip);
     }
