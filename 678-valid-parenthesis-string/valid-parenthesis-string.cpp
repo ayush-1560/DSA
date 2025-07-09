f9fb83dbbb36ce66e929e1210c9ab0b4b9807c82
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-09 22:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (20.06%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    bool solve(string s, int idx, int cnt,vector<vector<int>>&dp){
-        if(cnt<0) return false;
-        if(dp[idx][cnt]!=-1) return dp[idx][cnt];
-        if(idx==s.length()) return cnt==0;
-        if(s[idx]=='(') return  dp[idx][cnt]= solve(s,idx+1,cnt+1,dp);
-        else if(s[idx]==')') return  dp[idx][cnt]= solve(s,idx+1,cnt-1,dp);
-        else{
-             return dp[idx][cnt]= solve(s,idx+1,cnt+1,dp) || solve(s,idx+1,cnt-1,dp)
-            || solve(s,idx+1,cnt,dp);
-        }
-    }
     bool checkValidString(string s) {
-        int cnt=0;
-        int n = s.length();
-        vector<vector<int>>dp(n+1,vector<int>(2*n,-1));
-        return solve(s,0,cnt,dp);
+        stack<int>openSt,astSt;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                openSt.push(i);
+            }
+            else if(s[i]=='*'){
+                astSt.push(i);
+            }
+            else{
+                if(!openSt.empty()){
+                    openSt.pop();
+                }
+                else if(!astSt.empty()){
+                    astSt.pop();
+                }
+                else return false;
+            }
+        }
+        while(!openSt.empty() && !astSt.empty()){
+            if(openSt.top() > astSt.top()) return false;
+            openSt.pop();
+            astSt.pop();
+        }
+        return openSt.empty();
     }
 };
