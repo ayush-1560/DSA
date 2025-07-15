0a2a55076d06d7a4cc8ac4e15edc909545e74c58
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-15 11:56:01 +0530

    Time: 19 ms (6.24%) | Memory: 20.9 MB (12.40%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>> &adj, int u, vector<bool>& vis, vector<bool>& inRec) {
-        vis[u] = true;
-        inRec[u] = true;
-        for (int &v : adj[u]) {
-            if (!vis[v]) {
-                if (!dfs(adj, v, vis, inRec)) return false;  // FIX: Correct logic
-            }
-            else if (inRec[v]) return false;
-        }
-        inRec[u] = false;
-        return true;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> adj;
-        for (auto &course : prerequisites) {
-            adj[course[1]].push_back(course[0]);
+        unordered_map<int,vector<int>>adj;
+        vector<int>indegree(numCourses,0);
+        for(auto preq : prerequisites){
+            adj[preq[1]].push_back(preq[0]);
+            indegree[preq[0]]++;
         }
-        
-        vector<bool> vis(numCourses, false);
-        vector<bool> inRec(numCourses, false);
-        
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (!dfs(adj, i, vis, inRec)) return false;
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        int courseCnt=0;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            courseCnt++;
+            for(int v : adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0) q.push(v);
             }
         }
-        
-        return true;
+        return courseCnt==numCourses;
     }
 };
