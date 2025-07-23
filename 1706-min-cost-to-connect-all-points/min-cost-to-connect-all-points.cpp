ca15deb2437976c508b25010052f58b3b390c25a
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-23 17:28:34 +0530

    Time: 1060 ms (11.40%) | Memory: 251 MB (8.78%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,35 +1,52 @@
 class Solution {
 public:
-    typedef pair<int,int> P;
+     vector<int>parent;
+    vector<int>rank;
+    int find(int i){
+        if(i==parent[i]) return i;
+        else return parent[i] = find(parent[i]);
+    }
+    void Union(int x , int y){
+        if(rank[x]>rank[y]){
+            parent[y]=x;
+        }
+        else if(rank[y]>rank[x]){
+            parent[x]=y;
+        }
+        else {
+            parent[y]=x;
+            rank[x]++;
+        }
+    }
+    int kruskal(vector<vector<int>>&adj){
+        int sum = 0;
+        for(auto vec : adj){
+            int u = vec[0];
+            int v = vec[1];
+            int wt = vec[2];
+            int u_par = find(u);
+            int v_par = find(v);
+            if(u_par==v_par) continue;
+            Union(u_par,v_par);
+            sum+=wt;
+        }
+        return sum;
+    }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        unordered_map<int,vector<pair<int,int>>>adj;
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++) parent[i]=i;
+        vector<vector<int>>adj;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int dist = abs(points[j][0]-points[i][0])+abs(points[j][1]-points[i][1]);
-                adj[i].push_back({j,dist});
-                adj[j].push_back({i,dist});
+                adj.push_back({i,j,dist});
             }
         }
-        int cost =0;
-        vector<bool>vis(n,false);
-        priority_queue<P,vector<P>,greater<P>>pq;
-        pq.push({0,0});
-        while(!pq.empty()){
-            int dist1 = pq.top().first;
-            int u = pq.top().second;
-            pq.pop();
-            if(vis[u]) continue;
-            vis[u]=true;
-            cost+=dist1;
-            for(auto vec : adj[u]){
-                int v = vec.first;
-                int dist2 = vec.second;
-                if(!vis[v]){
-                    pq.push({dist2,v});
-                }
-            }
-        }
-        return cost;
+        sort(adj.begin(),adj.end(),[&](auto&v1,auto&v2){
+            return v1[2] < v2[2];
+        });
+        return kruskal(adj);
     }
 };
