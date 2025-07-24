e896c2710388d4d7ccffdcf1fdfff628dddb7914
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-24 21:24:38 +0530

    Time: 4 ms (16.90%) | Memory: 18 MB (6.24%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-    
+    vector<vector<int>>directions={{-1,0},{1,0},{0,1},{0,-1}};
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int, int>> q;
-        int onesCount = 0;
-        int minutes = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    onesCount++;
+        int n=grid[0].size();
+        int minutes=-1;
+        int fresh=0;
+        queue<pair<int,int>>q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2) {
+                    q.push({i,j});
                 }
+                else if(grid[i][j]==1) fresh++;
             }
         }
-        
-        while (!q.empty() && onesCount > 0) {
-            int size = q.size();
-            for (int k = 0; k < size; k++) {
-                int i = q.front().first;
-                int j = q.front().second;
+        if(fresh==0) return 0;
+        while(!q.empty()){
+            int sz=q.size();
+            minutes++;
+            while(sz--){
+                int x = q.front().first;
+                int y = q.front().second;
                 q.pop();
-                
-                for (auto dir : directions) {
-                    int i_ = i + dir[0];
-                    int j_ = j + dir[1];
-                    
-                    if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && grid[i_][j_] == 1) {
-                        grid[i_][j_] = 2;
-                        q.push({i_, j_});
-                        onesCount--;
+                for(auto dir : directions){
+                    int x_=x + dir[0];
+                    int y_=y + dir[1];
+                    if(x_>=0 && x_<m && y_>=0 && y_<n && grid[x_][y_]==1){
+                        grid[x_][y_]=2;
+                        fresh--;
+                        q.push({x_,y_});
                     }
                 }
             }
-            minutes++;
         }
-        
-        return (onesCount > 0) ? -1 : minutes;
+        return fresh==0 ? minutes : -1;
     }
 };
