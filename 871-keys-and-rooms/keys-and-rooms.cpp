6a1e2cec0ec257caede761d13e2a87bbc9c70cb8
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-25 11:45:30 +0530

    Time: 4 ms (12.96%) | Memory: 14.4 MB (89.41%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    void dfs(int u,unordered_map<int,vector<int>>&adj,vector<bool>&vis){
-        vis[u]=true;
-        for(int& v : adj[u]){
-            if(!vis[v]){
-                dfs(v,adj,vis);
+    void dfs(int u, vector<vector<int>>& rooms, vector<bool>& vis) {
+        vis[u] = true;
+        for (int v : rooms[u]) {
+            if (!vis[v]) {
+                dfs(v, rooms, vis);
             }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-        unordered_map<int,vector<int>>adj;
-        for(int u=0;u<n;u++){
-            for(int v : rooms[u]){
-              if(v!=u){
-                adj[u].push_back(v);
-              }
-            }
-        }
-        vector<bool>vis(n,false);
-        dfs(0,adj,vis);
+        vector<bool>vis(n,false); // no need of adj here
+        dfs(0,rooms,vis);
         for(int i=0;i<n;i++) if(!vis[i]) return false;
         return true;
     }
