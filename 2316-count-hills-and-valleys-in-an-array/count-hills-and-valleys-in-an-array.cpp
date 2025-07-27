690796e44a6b0a93b60f02204cccb749c476c983
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-07-27 11:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (3.77%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
+    bool isPeakOrValley(int a, int b, int c){
+        return (b > a && b > c) || (b < a && b < c);
+    }
+
     int countHillValley(vector<int>& nums) {
-        int n = nums.size();
-        int hills=0,valleys=0;
-        set<pair<int,int>>st;
-        for(int i=1;i<=n-2;i++){
-            int leftIdx=-1;
-            int rightIdx=n;
-            for(int j=i-1;j>=0;j--){
-                if(nums[j]<nums[i] || nums[j]>nums[i]){
-                    leftIdx=j;
-                    break;
-                }
-            }
-            for(int k=i+1;k<n;k++){
-                if(nums[k]<nums[i] || nums[k]>nums[i]){
-                    rightIdx=k;
-                    break;
-                }
-            }
-            if(leftIdx==-1 || rightIdx==n) continue;
-            if(nums[i]>nums[leftIdx] && nums[i]>nums[rightIdx]){
-                if(!st.count({leftIdx,rightIdx})){
-                    hills++;
-                    st.insert({leftIdx,rightIdx});
-                }
-            }
-            else if(nums[i]<nums[leftIdx] && nums[i]<nums[rightIdx]){
-                if(!st.count({leftIdx,rightIdx})){
-                    valleys++;
-                    st.insert({leftIdx,rightIdx});
-                }
-            }
+        vector<int> arr;
+        arr.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[i-1])
+                arr.push_back(nums[i]);
         }
-        return hills+valleys;
+
+        int cnt = 0;
+        for(int i = 1; i < arr.size() - 1; i++){
+            if(isPeakOrValley(arr[i-1], arr[i], arr[i+1]))
+                cnt++;
+        }
+        return cnt;
     }
 };
