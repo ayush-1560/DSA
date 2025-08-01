9337562c6b712aaece69abe0991cb0420fba1512
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-01 13:15:15 +0530

    Time: 93 ms (65.33%) | Memory: 76.6 MB (71.74%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    void dfs(int u,int ancestor,unordered_map<int,vector<int>>&adj,vector<vector<int>>&ans){
+    void dfs(int u,unordered_map<int,vector<int>>&adj,vector<bool>&vis){
+        vis[u]=true;
         for(int v : adj[u]){
-            if(ans[v].empty() || ans[v].back()!=ancestor){
-                ans[v].push_back(ancestor);
-                dfs(v,ancestor,adj,ans);
-            }
+            if(!vis[v]) dfs(v,adj,vis);
         }
     }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         unordered_map<int,vector<int>>adj;
-        vector<vector<int>>ans(n);
+        vector<vector<int>>ans;
         for(auto edge : edges){
-            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
-        for(int i=0;i<n;i++){
-            dfs(i,i,adj,ans);
+        for(int u=0;u<n;u++){
+            vector<int>ancestors;
+            vector<bool>vis(n,false);
+            dfs(u,adj,vis);
+            for(int i=0;i<n;i++){
+                if(i==u) continue;
+                if(vis[i]) ancestors.push_back(i);
+            }
+            ans.push_back(ancestors);
         }
         return ans;
     }
