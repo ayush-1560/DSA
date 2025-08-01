7e60b81a406406ad7aab8652df99bc898f56c5d5
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-01 13:53:40 +0530

    Time: 350 ms (34.91%) | Memory: 167.3 MB (9.60%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-    void dfs(int u,unordered_map<int,vector<int>>&adj,vector<bool>&vis){
-        vis[u]=true;
-        for(int v : adj[u]){
-            if(!vis[v]) dfs(v,adj,vis);
-        }
-    }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         unordered_map<int,vector<int>>adj;
         vector<vector<int>>ans;
+        vector<int> indegree(n, 0);
         for(auto edge : edges){
-            adj[edge[1]].push_back(edge[0]);
+            adj[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
         }
-        for(int u=0;u<n;u++){
-            vector<int>ancestors;
-            vector<bool>vis(n,false);
-            dfs(u,adj,vis);
-            for(int i=0;i<n;i++){
-                if(i==u) continue;
-                if(vis[i]) ancestors.push_back(i);
+        queue<int> que;
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                que.push(i);
             }
-            ans.push_back(ancestors);
+        }
+        vector<int> topoOrder;
+        while(!que.empty()) {
+            int curr = que.front();
+            que.pop();
+            topoOrder.push_back(curr);
+
+            for(int &v : adj[curr]) {
+                indegree[v]--;
+                if(indegree[v] == 0) {
+                    que.push(v);
+                }
+            }
+        }
+        vector<set<int>>vec(n);
+        for(int node : topoOrder){
+            for(int v : adj[node]){
+                vec[v].insert(node);
+                vec[v].insert(vec[node].begin(),vec[node].end());
+            }
+        }
+        for(int i=0;i<n;i++){
+            vector<int>v;
+            for(int ele : vec[i]) v.push_back(ele);
+            ans.push_back(v);
         }
         return ans;
     }
