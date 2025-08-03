78da94052370db1d76dc0f61c62f9328b09fb5e8
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-03 10:41:35 +0530

    Time: 271 ms (5.00%) | Memory: 65.7 MB (5.01%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int m,n;
+    vector<vector<int>>directions = {{-1,0},{1,0},{0,1},{0,-1}};
+    void dfs(int i, int j,vector<vector<int>>& grid){
+        grid[i][j]=0;
+        for(auto dir : directions){
+            int ni = i + dir[0];
+            int nj = j + dir[1];
+            if(ni>=0 && ni<m && nj>=0 && nj<n && grid[ni][nj]==1) dfs(ni,nj,grid);
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+         m = grid.size();
+         n = grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[0][j]==1) dfs(0,j,grid);
+                if(grid[i][0]==1) dfs(i,0,grid);
+                if(grid[i][n-1]==1) dfs(i,n-1,grid);
+                if(grid[m-1][j]==1) dfs(m-1,j,grid);
+            }
+        }
+        int enclaves = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) enclaves++;
+            }
+        }
+        return enclaves;
+    }
+};
