1f60ae8aea4d4ea4e796673a2528b7d37499badd
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-09 00:47:57 +0530

    Time: 136 ms (10.67%) | Memory: 428.1 MB (8.50%) - LeetSync

---
+++ 826-soup-servings/soup-servings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<pair<int,int>>serves ={{100,0},{75,25},{50,50},{25,75}};
+    double solve(int A,int B,vector<vector<double>>&dp){
+        if(A<=0 && B<=0) return 0.5;
+        if(A<=0) return 1.0;
+        if(B<=0) return 0.0;
+        if(dp[A][B]!=-1) return dp[A][B];
+        double prob = 0.0;
+        for(auto p : serves){
+            int A_taken = p.first;
+            int B_taken = p.second;
+            prob+=solve(A-A_taken,B-B_taken,dp);
+        }
+        return dp[A][B] = 0.25*prob;
+    }
+    double soupServings(int n) {
+        if(n>=5000) return 1;
+        vector<vector<double>>dp(n+1,vector<double>(n+1,-1));
+        return solve(n,n,dp);
+    }
+};
