c428c07ba06a9e47aff866fda93d73dcc1353a90
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-15 10:45:22 +0530

    Time: 138 ms (57.37%) | Memory: 8.9 MB (91.32%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int MOD = 1e9+7;
-    int dp[51][51][101];
-    int  solve(int idx,int n,int m, int k, int searchCost,int& maxSofar){
-        if(idx>=n) {
-            return (searchCost==k);
+    const int MOD = 1e9 + 7;
+    int dp[51][51][102];
+
+    int solve(int idx, int n, int m, int k, int searchCost, int maxSofar) {
+        if (idx == n) {
+            return (searchCost == k);
         }
-        if( maxSofar!=-1 && dp[idx][searchCost][maxSofar]!=-1) return dp[idx][searchCost][maxSofar];
-        int ans=0;
-        for(int i=1;i<=m;i++){
-            if(i > maxSofar){
-                ans = (ans +solve(idx+1,n,m,k,searchCost+1,i))%MOD;
+        if (dp[idx][searchCost][maxSofar+1] != -1) return dp[idx][searchCost][maxSofar+1];
+        
+        long long ans = 0;
+        for (int i = 1; i <= m; i++) {
+            if (i > maxSofar) {
+                ans += solve(idx + 1, n, m, k, searchCost + 1, i);
+            } else {
+                ans += solve(idx + 1, n, m, k, searchCost, maxSofar);
             }
-            else ans = (ans +solve(idx+1,n,m,k,searchCost,maxSofar))%MOD;
+            ans %= MOD;
         }
-        if(maxSofar!=-1) dp[idx][searchCost][maxSofar]=ans%MOD;
-        return ans%MOD;
+        return dp[idx][searchCost][maxSofar+1] = ans;
     }
+
     int numOfArrays(int n, int m, int k) {
-        int maxSofar=-1;
-        int searchCost=0;
-        memset(dp,-1,sizeof(dp));
-        return solve(0,n,m,k,searchCost,maxSofar);
+        memset(dp, -1, sizeof(dp));
+        return solve(0, n, m, k, 0, 0);
     }
 };
