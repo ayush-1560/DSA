f326d2ce462dcbc7a88f28dc2877d5739d17332a
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-15 16:06:19 +0530

    Time: 8 ms (83.41%) | Memory: 12.7 MB (48.42%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int>& arr) {
-        int n=arr.size();
-        sort(arr.begin(),arr.end());
-        vector<int> dp(n, 1);
-        vector<int> hash(n, 1);
-
-        for (int i = 0; i < n; i++) {
-                hash[i] = i;
-                for (int prev_index = 0; prev_index < i; prev_index++) {
-                if (arr[i] % arr[prev_index] == 0 && 1 + dp[prev_index] > dp[i]) {
-                    dp[i] = 1 + dp[prev_index];
-                    hash[i] = prev_index;
+    
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>dp(n,1);
+        vector<int>prev_idx(n,-1);
+        int lastIdx = 0;
+        int maxLen = 1;
+        for(int i=0;i<n;i++){
+            vector<int>temp;
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0){
+                    if(dp[i]<dp[j]+1){
+                        dp[i] = dp[j]+1;
+                        prev_idx[i]=j;
+                    }
+                    if(dp[i]>maxLen){
+                        maxLen = dp[i];
+                        lastIdx=i;
+                    }
                 }
             }
+            
         }
-
-        int ans = -1;
-        int lastIndex = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (dp[i] > ans) {
-                ans = dp[i];
-                lastIndex = i;
-            }
+        vector<int>ans;
+        while(lastIdx!=-1){
+            ans.push_back(nums[lastIdx]);
+            lastIdx = prev_idx[lastIdx];
         }
-
-        vector<int> temp;
-        temp.push_back(arr[lastIndex]);
-        while (hash[lastIndex] != lastIndex) {
-            lastIndex = hash[lastIndex];
-            temp.push_back(arr[lastIndex]);
-        }
-        reverse(temp.begin(), temp.end());
-
-        return temp;
+        return ans;
     }
 };
