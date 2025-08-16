108a4f0907bd72548d804c5ef39bb640fe7fcf60
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-16 17:50:09 +0530

    Time: 30 ms (11.53%) | Memory: 26.9 MB (43.47%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = 0;
-    }
-    for (int i = 0; i <= m; i++) {
-        dp[0][i] = 0;
-    }
-    for (int ind1 = 1; ind1 <= n; ind1++) {
-        for (int ind2 = 1; ind2 <= m; ind2++) {
-            if (str1[ind1 - 1] == str2[ind2 - 1])
-                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
-            else
-                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+        int m = str1.length();
+        int n = str2.length();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++){
+           for(int j=0;j<=n;j++){
+              if(i==0 || j==0) dp[i][j] = i+j;
+              else if(str1[i-1]==str2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+              else dp[i][j] = 1 + min(dp[i-1][j],dp[i][j-1]);
+           }
         }
-    }
-    int i=n;
-    int j=m;
-    string ans="";
-    while(i>0 and j>0){
-        if(str1[i-1]==str2[j-1]){
-            ans+=str1[i-1];
+        string scs="";
+        int i=m,j=n;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                scs.push_back(str1[i-1]);
+                i--;
+                j--;
+            }
+            else {
+                if(dp[i-1][j] < dp[i][j-1]){
+                    scs+=str1[i-1];
+                    i--;
+                }
+                else {
+                    scs+=str2[j-1];
+                    j--;
+                }
+            }
+        }
+        while(i>0){
+            scs+=str1[i-1];
             i--;
+        }
+        while(j>0){
+            scs+=str2[j-1];
             j--;
         }
-        else if(dp[i-1][j]>dp[i][j-1]){
-            ans+=str1[i-1];
-            i--;
-        }
-        else{
-            ans+=str2[j-1];
-            j--;
-        }
+        reverse(scs.begin(),scs.end());
+        return scs;
     }
-    while(i>0){
-        ans+=str1[i-1];
-        i--;
-    }
-    while(j>0){
-        ans+=str2[j-1];
-        j--;
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
-}
 };
