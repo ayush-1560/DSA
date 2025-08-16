02419c516904ed40bcc84eb227721fd096b6126f
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-16 08:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (71.54%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string str = to_string(num);
-        for(int i=0;i<str.length();i++){
-            if(str[i]=='6'){
-                str[i]='9';
+        for(char & ch : str){
+            if(ch=='6'){
+                ch='9';
                 break;
             }
         }
