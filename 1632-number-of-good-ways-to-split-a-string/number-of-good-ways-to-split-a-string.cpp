d3491f9a656f3c1de2a097c84a37ab97c2840c4f
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-17 09:45:04 +0530

    Time: 11 ms (60.82%) | Memory: 15.4 MB (29.16%) - LeetSync

--- 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
@@ -3,18 +3,16 @@ public:
     int numSplits(string s) {
         int n = s.length();
         int cnt=0;
-        vector<int>pref(n,0);
-        vector<int>suff(n,0);
-        unordered_set<int>prefSet;
-        unordered_set<int>suffSet;
-        for(int i=0;i<n;i++){
-            prefSet.insert(s[i]);
-            suffSet.insert(s[n-i-1]);
-            pref[i]=prefSet.size();
-            suff[n-i-1]=suffSet.size();
+        unordered_set<int>st1,st2;
+        vector<int>pre(n,0),suff(n,0);
+        for(int i=0;i<s.size();i++){
+            st1.insert(s[i]-'a');
+            st2.insert(s[n-i-1]-'a');
+            pre[i]=st1.size();
+            suff[n-i-1]=st2.size();
         }
-        for(int i=1;i<n;i++){
-            if(pref[i-1]==suff[i]) cnt++;
+        for(int i=1;i<pre.size();i++){
+            if(pre[i-1]==suff[i]) cnt++;
         }
         return cnt;
     }
