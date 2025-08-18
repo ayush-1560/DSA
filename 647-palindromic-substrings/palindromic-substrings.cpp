c7c1f82471fad2dced02e96fd11b92e781a2e9af
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-18 15:11:53 +0530

    Time: 33 ms (29.70%) | Memory: 12.9 MB (44.72%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    bool check(string& s,int i, int j){
+        if(i>=j) return true;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==s[j]) return dp[i][j]= check(s,i+1,j-1);
+        return dp[i][j] = false;
+    }
+    int countSubstrings(string s) {
+        memset(dp,-1,sizeof(dp));
+        int cnt=0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(check(s,i,j)) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
