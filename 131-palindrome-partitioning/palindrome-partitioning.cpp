55218419f736412d42d7593390eb6c38376a8c4b
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-20 09:42:13 +0530

    Time: 30 ms (90.89%) | Memory: 53.4 MB (93.98%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    bool isPall(string s, int i , int j){
-        while(i < j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-
-    void solve(int idx, string s, vector<string>& v, vector<vector<string>>& ans){
-        if(idx >= s.length()){
-            ans.push_back(v);
+    void solve(int i,string&s,vector<string>&part,vector<vector<string>>&ans,vector<vector<bool>>&dp){
+        if(i>=s.length()){
+            ans.push_back(part);
             return;
         }
-        for(int i = idx; i < s.length(); i++){
-            if(isPall(s, idx, i)){
-                v.push_back(s.substr(idx, i - idx + 1));
-                solve(i + 1, s, v, ans);
-                v.pop_back();
+        for(int j=i;j<s.length();j++){
+            if(dp[i][j]) {
+                part.push_back(s.substr(i,j-i+1));
+                solve(j+1,s,part,ans,dp);
+                part.pop_back();
             }
         }
     }
-
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> v;
-        solve(0, s, v, ans);
+        int n = s.length();
+        vector<vector<bool>>dp(n+1,vector<bool>(n+1,false));
+        for(int L =1 ; L<=n;L++){
+            for(int i=0;i+L-1<n;i++){
+                int j=i+L-1;
+                if(i==j) dp[i][i]=true;
+                else if(s[i]==s[j]){
+                    if(L==2) dp[i][j]=true;
+                    else dp[i][j] = dp[i+1][j-1];
+                }
+            }
+        }
+        vector<vector<string>>ans;
+        vector<string>part;
+        solve(0,s,part,ans,dp);
         return ans;
     }
 };
