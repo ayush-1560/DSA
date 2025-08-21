1f65cdb3bb01c84ca193aab56432aace19b92d9f
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-21 15:34:09 +0530

    Time: 1 ms (26.55%) | Memory: 13.1 MB (47.92%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int dp[201][201];
-    int m, n;
-
-    int solve(int i, int j, vector<vector<int>>& grid) {
-        if(i>=m || j>=n) return 1e9;
-        if(i==m-1 && j==n-1) {
-            return grid[i][j] > 0 ? 1 : abs(grid[i][j]) + 1;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int right = solve(i, j+1, grid);
-        int down  = solve(i+1, j, grid);
-
-        int ans = min(right, down) - grid[i][j];
-        return dp[i][j] = ans <= 0 ? 1 : ans;
-    }
-
+    
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        memset(dp,-1,sizeof(dp));
-        m = dungeon.size();
-        n = dungeon[0].size();
-        return solve(0,0,dungeon);
+        int m = dungeon.size();
+        int n = dungeon[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1){
+                    dp[i][j] = dungeon[i][j] < 0 ? abs(dungeon[i][j])+1 : 1;
+                }
+                else{
+                    int down = (i+1>=m) ? 1e9 : dp[i+1][j];
+                    int right = (j+1>=n) ? 1e9 : dp[i][j+1];
+                    int res = min(down,right) - dungeon[i][j];
+                    dp[i][j] = res<=0 ? 1 : res;
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
