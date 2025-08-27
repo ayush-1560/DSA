bed89bf0549e0ae6cdecaac9fde93e882728f63b
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-27 10:22:30 +0530

    Time: 110 ms (86.63%) | Memory: 145.6 MB (79.78%) - LeetSync

---
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& nums) {
+        int n = nums.size();
+        int maxLen = 1;
+        vector<vector<int>> dp(n, vector<int>(1001, 0));
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j] + 500;
+                dp[i][diff] = dp[j][diff] > 0 ? dp[j][diff] + 1 : 2;
+                maxLen = max(maxLen, dp[i][diff]);
+            }
+        }
+        return maxLen;
+    }
+};
