9f68a350b43b20722ce6be6edfdd0afeb0fef6fd
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-27 21:50:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (98.01%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,11 +4,10 @@ public:
         int n = nums.size();
         int i=0,j=0;
         while(j<n){
-            if(nums[j]!=0){
-                swap(nums[i],nums[j]);
-                i++;
-            }
+            while(i<n && nums[i]!=0) i++;
+            if( i<j && nums[j]!=0) swap(nums[i],nums[j]);
             j++;
         }
+        
     }
 };
