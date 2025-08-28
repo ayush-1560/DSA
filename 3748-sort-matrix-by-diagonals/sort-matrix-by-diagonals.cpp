25d8571a7b2f3142a4a7dad26109b1cb4e54d71a
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-08-28 10:04:48 +0530

    Time: 10 ms (55.76%) | Memory: 46.3 MB (17.70%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                mp[i-j].push_back(grid[i][j]);
+        int n = grid.size();
+
+        unordered_map<int, vector<int>> mp;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int diag = i-j;
+                mp[diag].push_back(grid[i][j]);       
             }
         }
-        for(auto& it : mp){
-            if(it.first>=0) sort(it.second.rbegin(),it.second.rend());
-            else sort(it.second.begin(),it.second.end());
-        }
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                grid[i][j] = mp[i-j].back();
-                mp[i-j].pop_back();
+
+        for(auto &it : mp) {
+            if(it.first >= 0) {
+                sort(begin(it.second), end(it.second));
+            } else {
+                sort(rbegin(it.second), rend(it.second));
             }
         }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int diag = i-j;
+                grid[i][j] = mp[diag].back();
+                mp[diag].pop_back();      
+            }
+        }
+
         return grid;
     }
 };
