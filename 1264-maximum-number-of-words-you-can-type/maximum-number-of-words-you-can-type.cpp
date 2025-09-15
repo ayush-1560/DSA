67a9b77c3c514be74a57eba7daeda342c18634b1
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-09-15 07:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (29.57%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
+        unordered_set<char>st(brokenLetters.begin(),brokenLetters.end());
         stringstream ss(text);
         string word;
         int words = 0;
         while(ss>>word){
             bool canType=true;
             for(char ch : word){
-                if(brokenLetters.find(ch)!=string::npos){
+                if(st.count(ch)){
                     canType = false;
                     break;
                 }
