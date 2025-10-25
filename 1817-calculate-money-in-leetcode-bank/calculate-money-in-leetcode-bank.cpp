e4740fecb3a7f5bca70ffc9569620d7566b5a9be
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-10-25 16:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.24%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int ans = 0;
-        int iv = 1;
-        while(n>0){
-            int sv=iv;
-            for(int i=1;i<=min(n,7);i++){
-                ans+=sv;
-                sv++;
-            }
-            n-=7;
-            iv++;
+        int n1 = n/7;
+        int n2= n%7;
+        int ans =0;
+        ans+=n1*(2*28 + (n1-1)*7)/2;
+        int sv = n1+1;
+        for(int i=1;i<=n2;i++){
+            ans+=sv;
+            sv++;
         }
         return ans;
     }
