e3ba89138d0269495421cd5dfe3b166dcc0f87c8
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-11-14 07:53:54 +0530

    Time: 1091 ms (11.91%) | Memory: 91.3 MB (47.34%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>>ans(n,vector<int>(n,0));
-        for (auto query : queries){
-            int l1=query[0];
-            int l2=query[2];
-            int r1=query[1];
-            int r2=query[3];
-            for(int i=l1;i<=l2;i++){
-                for(int j=r1;j<=r2;j++){
-                    ans[i][j]++;
-                }
+       vector<vector<int>>mat(n,vector<int>(n,0));
+       for(auto query : queries){
+           int x1 = query[0];
+           int y1 = query[1];
+           int x2 = query[2];
+           int y2 = query[3];
+           for(int i=x1;i<=x2;i++){
+            for(int j=y1;j<=y2;j++){
+                mat[i][j]++;
             }
-        }
-        return ans;
+           }
+       }
+       return mat; 
     }
 };
