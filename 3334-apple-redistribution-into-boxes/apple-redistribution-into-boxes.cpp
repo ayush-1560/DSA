c0843f10e07add28b605ce92b63c3f6efdb3d473
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-12-24 09:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 33.1 MB (77.58%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
-        int sum1 = accumulate(apple.begin(),apple.end(),0);
-        int sum2 = accumulate(capacity.begin(),capacity.end(),0);
+        int sum = accumulate(apple.begin(),apple.end(),0);
         sort(capacity.begin(),capacity.end(),greater<int>());
         int ans = 0;
-        int i=0;
-        while(i<capacity.size() && sum1>0){
-            sum1-=capacity[i];
+        while(sum>0){
+            sum-=capacity[ans];
             ans++;
-            i++;
         }
         return ans;
     }
