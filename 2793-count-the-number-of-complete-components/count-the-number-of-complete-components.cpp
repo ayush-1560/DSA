c908e16e70cb753e50144e36c259be078afd4d1e
Ayush <139117702+ayush-1560@users.noreply.github.com>
2026-07-11 09:49:02 +0530

    Time: 86 ms (14.57%) | Memory: 139.1 MB (21.17%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,30 +1,31 @@
 class Solution {
-public:
-    void dfs(unordered_map<int,vector<int>>&adj,int node,vector<bool>&vis,int& v , int& e){
-        vis[node]=true;
-        v++;
-        e+=adj[node].size();
-        for(auto ngbr : adj[node]){
-            if(!vis[ngbr]){
-                dfs(adj,ngbr,vis,v,e);
-            }
-        }
+void dfs(int node,unordered_map<int,vector<int>>&adj,vector<bool>&vis,int& v, int& e){
+    vis[node] = true;
+    v++;
+    e+=adj[node].size();
+    for(auto ngbr : adj[node]){
+        if(!vis[ngbr]) dfs(ngbr,adj,vis,v,e);
     }
+}
+public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<bool>vis(n,false);
         unordered_map<int,vector<int>>adj;
+        vector<bool>vis(n,false);
+        int ans = 0;
+
         for(auto edge : edges){
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
-        int ans=0;
+
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                int v=0,e=0;
-                dfs(adj,i,vis,v,e);
-                if((v*(v-1))==e) ans++;
+            if(!vis[i]) {
+                int v = 0,e=0;
+                dfs(i,adj,vis,v,e);
+                if(e==v*(v-1)) ans++;
             }
         }
+
         return ans;
     }
 };
