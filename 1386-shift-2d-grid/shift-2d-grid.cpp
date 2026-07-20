dc52f81310131de3e594c12aa005e22205d7d0e3
Ayush <139117702+ayush-1560@users.noreply.github.com>
2026-07-21 00:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (19.31%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -1,17 +1,41 @@
 class Solution {
+void reverse(vector<int>&arr,int i, int j){
+    while(i<j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+}
+void rotate(vector<int>&arr,int k){
+    int n = arr.size();
+    if(k==0) return;
+    k = k%n;
+    reverse(arr,0,n-1);
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+}
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>ans(m,vector<int>(n));
+
+        vector<int>arr;
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int newVal = ((i*n + j)+k)%(m*n);
-                int newRow = newVal/n;
-                int newCol = newVal%n;
-                ans[newRow][newCol] = grid[i][j];
+                arr.push_back(grid[i][j]);
             }
         }
-        return ans;
+
+        rotate(arr,k);
+        int t = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                grid[i][j] = arr[t];
+                t++;
+            }
+        }
+
+        return grid;
     }
 };
