866e0cc16271a5d1ba267c8663c992fc0d323321
Ayush <139117702+ayush-1560@users.noreply.github.com>
2026-07-25 10:40:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (99.06%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,20 +1,16 @@
 class Solution {
-vector<int>findValues(int n){
-    int max = -1, smax = -1;
-    while(n>0){
-        int d = n%10;
-        if(d>max){
-            smax = max;
-            max = d;
-        }
-        else if(d>smax) smax = d;
-        n/=10;
-    }
-    return {max,smax};
-}
 public:
     int maxProduct(int n) {
-        vector<int>values = findValues(n);
-        return values[0]*values[1];
+        int max = -1, smax = -1;
+        while (n > 0) {
+            int d = n % 10;
+            if (d > max) {
+                smax = max;
+                max = d;
+            } else if (d > smax)
+                smax = d;
+            n /= 10;
+        }
+        return max * smax;
     }
 };
