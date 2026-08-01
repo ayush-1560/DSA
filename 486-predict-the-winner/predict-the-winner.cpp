ac04f0b3aedaea7032c01cb0bb13f4566bb3c31d
Ayush <139117702+ayush-1560@users.noreply.github.com>
2026-08-01 08:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -3,6 +3,7 @@ int dp[21][21];
 int solve(vector<int>& nums,int i, int j){
     if(i>j) return 0;
     if(i==j) return nums[i];
+    if(dp[i][j]!=-1) return dp[i][j];
     int take_i = nums[i] + min(solve(nums,i+2,j),solve(nums,i+1,j-1));
     int take_j = nums[j] + min(solve(nums,i+1,j-1),solve(nums,i,j-2));
     return dp[i][j] = max(take_i,take_j);
