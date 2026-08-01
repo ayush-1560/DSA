d451ae927053e63d07cf6a424bb8c8119543dc23
Ayush <139117702+ayush-1560@users.noreply.github.com>
2026-08-01 08:35:40 +0530

    Time: 81 ms (25.44%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,17 +1,18 @@
 class Solution {
+int dp[21][21];
+int solve(vector<int>& nums,int i, int j){
+    if(i>j) return 0;
+    if(i==j) return nums[i];
+    int take_i = nums[i] + min(solve(nums,i+2,j),solve(nums,i+1,j-1));
+    int take_j = nums[j] + min(solve(nums,i+1,j-1),solve(nums,i,j-2));
+    return dp[i][j] = max(take_i,take_j);
+}
 public:
-    int solve(vector<int>& nums,int i, int j){
-        if(i>j) return 0;
-        if(i==j) return nums[i];
-        int take_i = nums[i] + min(solve(nums,i+2,j),solve(nums,i+1,j-1));
-        int take_j = nums[j] + min(solve(nums,i+1,j-1),solve(nums,i,j-2));
-        return max(take_i,take_j);
-    }
     bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-        int total_score = accumulate(nums.begin(),nums.end(),0);
-        int player1_score = solve(nums,0,n-1);
-        int player2_score = total_score - player1_score;
-        return player1_score >= player2_score;
+        memset(dp,-1,sizeof(dp));
+        int totalScore = accumulate(nums.begin(),nums.end(),0);
+        int player1Score = solve(nums,0,nums.size()-1);
+        int player2Score = totalScore - player1Score;
+        return player1Score >= player2Score;
     }
 };
